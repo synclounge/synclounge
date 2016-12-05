1d2f0cda19f2cdf7e1c0d33a1c9e666917a8436f
pureMidi <cordalis.g@gmail.com>
2016-12-05 12:13:24 +1100

    Compatibility for server/room workflow process.

--- app/js/settings.js
+++ app/js/settings.js
@@ -56,6 +56,11 @@ function ServerSelectedChanged(){
     console.log(dropdown.options[dropdown.selectedIndex].value)
     if (dropdown.options[dropdown.selectedIndex].value == 'custom') {
         $('#customField').removeClass('hide')
+    var el = $("input:text").get(0);
+        var elemLen = el.value.length;
+        el.selectionStart = elemLen;
+        el.selectionEnd = elemLen;
+        document.getElementById('ptServerCustom').focus() 
     } else {
         connectToPTServer(dropdown.options[dropdown.selectedIndex].value)
     }
