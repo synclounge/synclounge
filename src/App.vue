8ed590f64d82c5f013e60d120ed969f30d404fcd
samcm <sam_cm_to@hotmail.com>
2017-05-11 23:44:45 +1000

    Removed meta viewport tag

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app" v-bind:class="{ 'dark-theme': darkMode }" style="height: 100%; overflow-y: hidden">
-  <meta name="viewport" content="width=device-width">
     <navbar></navbar>
     <router-view></router-view>
   </div>
