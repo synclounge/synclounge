39406626587ba52795aa5d1ce7d8ed1a20e6741c
samcm <sam_cm_to@hotmail.com>
2017-06-03 21:54:40 +1000

    Preferences Styling

--- src/components/application/settings.vue
+++ src/components/application/settings.vue
@@ -6,12 +6,12 @@
             </div>
         </div>
         <div class="row" style="margin-bottom: 0;">
-            <div class="col l1 s1">
+            <div class="col l2 s1">
                 <h4><i class="material-icons"  title="Sets how frequently Plex Together will poll the client for new information. 
                 Most clients can handle multiple requests per second but mobile devices might not be able to handle the load. Default is 5000ms (5 seconds)">info_outline</i></h4>
             </div>
-            <div class="col l6 s11">
-                <h4 style="padding-bottom: 10px">Plex Client Polling Interval</h4>
+            <div class="col l5 s11">
+                <h4 style="padding-bottom: 10px; text-align: left;">Plex Client Polling Interval</h4>
             </div>
             <div class="col l3 s12">
                 <form action="#" style="margin-top: -15px">
@@ -26,11 +26,11 @@
         </div>
 
         <div class="row" style="margin-bottom: 0;">
-            <div class="col l1 s1">
+            <div class="col l2 s1">
                 <h4><i class="material-icons" title="Sets the acceptable distance away from the host in milliseconds. Syncs below 1500ms may be hard to achieve. Default is 3000ms(3 seconds).">info_outline</i></h4>
             </div>
-            <div class="col l6 s11">
-                <h4 style="padding-bottom: 10px">Sync Flexibility </h4>
+            <div class="col l5 s11">
+                <h4 style="padding-bottom: 10px; text-align: left;">Sync Flexibility </h4>
             </div>
             <div class="col l3 s12">
                 <form action="#" style="margin-top: -15px">
@@ -45,20 +45,20 @@
         </div>
         
         <div class="row" style="margin-bottom: 0;">
-            <div class="col l1 s1">
+            <div class="col l2 s1">
                 <h4><i class="material-icons" title="Sets the syncing method used when we need to get back in line with the host. 'Clean seek' seeks straight to the host. 'Skip ahead' skips 10 seconds ahead of the host, pauses and resumes when it thinks the host is there. Use 'Skip ahead' if buffering content on the client isn't close to instant.">info_outline</i></h4>
             </div>
             <div class="col l5 s11">
-                <h4 style="padding-bottom: 10px">Syncing Method</h4>
+                <h4 style="padding-bottom: 10px; text-align: left;">Syncing Method</h4>
             </div>
             <form action="#">
-                <div class="input-field col l3 s6">
+                <div class="input-field col l2 s6">
                     <input name="syncModeInputs" id="CLEANSEEK" value="cleanseek" v-model="syncmode" type="radio" />
                     <label for="CLEANSEEK">Clean Seek</label>
 
                     <!-- <v-radio :name="syncModeInputs" id="CLEANSEEK" val="cleanseek" v-model="syncmode">Clean Seek</v-radio> -->
                 </div>            
-                <div class="input-field col l3 s6">
+                <div class="input-field col l2 s6">
                     <input name="syncModeInputs" id="SKIPAHEAD" value="skipahead" v-model="syncmode" type="radio" />
                     <label for="SKIPAHEAD">Skip Ahead</label>
                     <!-- <v-radio :name="syncModeInputs" id="SKIPAHEAD" val="skipahead" v-model="syncmode" style="margin: 0">Skip Ahead</v-radio>-->
