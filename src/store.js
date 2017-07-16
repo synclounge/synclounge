81c4f62d8daeb4f4490d0e975732823d734f7438
samcm <sam_cm_to@hotmail.com>
2017-07-17 03:31:54 +1000

    Version Inc

--- src/store.js
+++ src/store.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV == 'development') {
 const state = {
   count: 0,
   appTitle: 'PlexTogether',
-  appVersion: '1.2.4',
+  appVersion: '1.2.5',
   background: null,
   shownChat: false,
   plex: null,
