a648f285345f03d2d7a8b9e7652dcd4830d0674b
Sam <sam@puritydev.io>
2018-07-20 22:39:36 +1000

    Verbose settings logging

--- SettingsHelper.js
+++ SettingsHelper.js
@@ -24,6 +24,8 @@ module.exports = function () {
   for (let i = 0; i < fields.length; i++) {
     let setting = fields[i]
     output[setting] = args[setting] || process.env[setting] || defaults[setting]
+    process.env[setting] = output[setting]
   }
+  console.log('Our settings are', output)
   return output
 }
