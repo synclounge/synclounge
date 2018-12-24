ef60fa4b315d064be78466fc03c582ead5ffacb0
Sam Calder-Mason <sam@puritydev.io>
2018-12-24 17:10:59 +1100

    Library item styling

--- src/components/application/plexbrowser/plexlibrary.vue
+++ src/components/application/plexbrowser/plexlibrary.vue
@@ -1,12 +1,12 @@
 <template>
   <span style="max-height: 90%">
-    <v-layout v-if="!contents && !browsingContent" row justify-center align-center>
+    <v-layout v-if="!contents && !browsingContent" row justify-center align-start>
       <v-flex xs12 style="position:relative">
         <v-progress-circular style="left: 50%; top:50%" v-bind:size="60" indeterminate class="amber--text"></v-progress-circular>
       </v-flex>
     </v-layout>
     <div v-if="!browsingContent && contents" class="mt-3" style="height:90vh; overflow-y: auto">
-      <v-layout class="row" row wrap align-center justify-center>
+      <v-layout class="row" row wrap>
         <v-flex xs3 sm3 md1 lg1  class="ma-1"  v-for="content in contents.MediaContainer.Metadata" :key="content.key">
           <plexthumb :content="content" :server="server" type="thumb" style="margin:7%" @contentSet="setContent(content)"></plexthumb>
         </v-flex>
