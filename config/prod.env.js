f11864f5dbc8d4c6c19a2b50fa6346fd17db1309
Sam Calder-Mason <sam.caldermason@neds.com>
2019-04-20 18:26:08 +1000

    logging

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,9 @@ const git = require('git-rev-sync');
 const settings = new (require('../SettingsHelper'))();
 
 console.log('Production settings', settings);
+if (process.env.API_OVERRIDE) {
+  console.log('Building with API_OVERRIDE', process.env.API_OVERRIDE);
+}
 
 module.exports = {
   NODE_ENV: '"production"',
