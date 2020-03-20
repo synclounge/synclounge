ef89eabcc58d313e3ac3f78dc186e205e6c945e1
PaulMH <paul@blendlabs.com>
2020-03-20 09:27:26 -0700

    update to better filter clients

--- src/store/modules/plex/actions.js
+++ src/store/modules/plex/actions.js
@@ -80,7 +80,7 @@ export default {
           for (var index in result.MediaContainer.Device) {
             const device = result.MediaContainer.Device[index].$;
             const connections = result.MediaContainer.Device[index].Connection;
-            if (device.provides.indexOf('player') > -1 && device.product != "Plex Web" && connections.length > 0) {
+            if (device.provides.indexOf('player') > -1 && device.product != "Plex Web" && connections && connections.length > 0) {
               console.log(client)
               client--;
               const tempConnectionsArray = [];
