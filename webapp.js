1be26305c80c12599174761a85ea32b81bf865d5
Travis Shivers <ttshivers@gmail.com>
2020-06-06 20:35:44 -0500

    Grab cleaned up webapp from my vuecli branch

--- webapp.js
+++ webapp.js
@@ -8,77 +8,83 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const Waterline = require('waterline');
-const WaterlineMysql = require('waterline-mysql');
+const SailsMysql = require('sails-mysql');
 const SailsDisk = require('sails-disk');
 
 const { readSettings } = require('./SettingsHelper');
 
 const settings = readSettings();
 
-let PORT = 8088;
+const bootstrap = () => {
+  if (!settings.accessUrl) {
+    console.log(
+      'Missing required argument `accessUrl`. This URL is used for redirecting invite links. See documentation for how to set this',
+    );
+    return Promise.reject(new Error('Missing URL for invite links'));
+  }
 
-const bootstrap = () =>
-  new Promise(async (resolve, reject) => {
-    if (!settings.accessUrl) {
-      console.log(
-        'Missing required argument `accessUrl`. This URL is used for redirecting invite links. See documentation for how to set this',
-      );
-      return reject(new Error('Missing URL for invite links'));
-    }
-    if (!settings.webapp_port) {
-      console.log('Defaulting webapp to port 8088');
-    } else {
-      PORT = settings.webapp_port;
-    }
-    PORT = parseInt(PORT, 10);
-    const baseSettings = require('./waterline_settings.json');
-    // console.log('Basesettings', baseSettings);
-    baseSettings.waterline.adapters = {
-      'waterline-mysql': WaterlineMysql,
-      'sails-disk': SailsDisk,
+  const baseSettings = require('./waterline_settings.json');
+  // console.log('Basesettings', baseSettings);
+  baseSettings.waterline.adapters = {
+    'sails-mysql': SailsMysql,
+    'sails-disk': SailsDisk,
+  };
+
+  baseSettings.waterline.datastores = baseSettings.database.datastores;
+  baseSettings.waterline.models.invite.beforeCreate = (data, cb) => {
+    console.log('Creating Invite', data);
+    const params = {
+      server: data.server,
+      room: data.room,
+      owner: data.owner,
     };
-    baseSettings.waterline.datastores = baseSettings.database.datastores;
-    baseSettings.waterline.models.invite.beforeCreate = async (data, cb) => {
-      console.log('Creating Invite', data);
-      const params = {
-        server: data.server,
-        room: data.room,
-        owner: data.owner,
-      };
-      if (data.password) {
-        params.password = data.password;
-      }
+    if (data.password) {
+      params.password = data.password;
+    }
 
-      const query = Object.entries(params)
-        .map(([key, value]) => `${encodeURIComponent(key)}=${value}`)
-        .join('&');
+    const query = Object.entries(params)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${value}`)
+      .join('&');
 
-      const fullUrl = `${settings.accessUrl}/#/join?${query}`;
-      data.fullUrl = fullUrl;
-      data.code = (0 | (Math.random() * 9e6)).toString(36);
-      cb();
-    };
+    // TODO: ask why we require accessUrl and not just redirect relatively
+    const fullUrl = `${settings.accessUrl}/#/join?${query}`;
+    data.fullUrl = fullUrl;
+    // eslint-disable-next-line no-bitwise
+    data.code = (0 | (Math.random() * 9e6)).toString(36);
+    cb();
+  };
+
+  return new Promise((resolve, reject) => {
     Waterline.start(baseSettings.waterline, (err, orm) => {
       if (err) {
         return reject(err);
       }
-      resolve(orm);
+      return resolve(orm);
     });
   });
+};
 
-const app = async (orm) => {
-  const root = express();
+const webApp = async (orm) => {
+  const app = express();
+  const router = express.Router();
+
   // Setup our web app
-  root.use(cors());
-  root.use(bodyParser.json());
-  root.use(
+  app.use(cors());
+
+  // Allow body to be json or urlencoded
+  app.use(bodyParser.json());
+  app.use(
     bodyParser.urlencoded({
       extended: true,
     }),
   );
-  root.use(`${settings.webroot}/`, express.static(path.join(__dirname, 'dist')));
+
+  app.use(settings.webroot, router);
+
+  router.use('/', express.static(path.join(__dirname, 'dist')));
+
   // Invite handling
-  root.get(`${settings.webroot}/invite/:id`, async (req, res) => {
+  router.get('/invite/:id', async (req, res) => {
     console.log('handling an invite', req.params.id);
     const shortObj = await Waterline.getModel('invite', orm).findOne({ code: req.params.id });
     console.log('Invite data', shortObj);
@@ -88,7 +94,8 @@ const app = async (orm) => {
     console.log('Redirecting an invite link', shortObj);
     return res.redirect(shortObj.fullUrl);
   });
-  root.post(`${settings.webroot}/invite`, async (req, res) => {
+
+  router.post('/invite', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     if (!req.body) {
       return res
@@ -98,23 +105,29 @@ const app = async (orm) => {
         })
         .end();
     }
-    const data = {};
+
     const fields = ['server', 'room', 'password', 'owner'];
-    for (let i = 0; i < fields.length; i++) {
-      if (req.body[fields[i]] === undefined) {
-        return res
-          .send({
-            success: false,
-            msg: `ERR: You did not specify ${fields[i]}`,
-            field: fields[i],
-          })
-          .end();
-      }
-      data[fields[i]] = req.body[fields[i]];
+
+    const missingField = fields.find((field) => req.body[field] === undefined);
+    if (missingField !== undefined) {
+      return res
+        .send({
+          success: false,
+          msg: `ERR: You did not specify ${missingField}`,
+          field: missingField,
+        })
+        .end();
     }
+
+    const data = fields.reduce((dataAcc, field) => {
+      dataAcc[field] = req.body[field];
+      return dataAcc;
+    }, {});
+
     const result = await Waterline.getModel('invite', orm)
       .create({ ...data })
       .fetch();
+
     return res
       .send({
         url: `${settings.accessUrl}/invite/${result.code}`,
@@ -124,36 +137,40 @@ const app = async (orm) => {
       })
       .end();
   });
+
   // Config handling
-  root.get(`${settings.webroot}/config`, (req, res) => {
+  router.get('/config', (req, res) => {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');
     res.send(settings);
   });
+
   // Catch anything else and redirect to the base URL
-  root.get('*', (req, res) => {
+  router.get('*', (req, res) => {
     console.log('Catch all:', req.url);
     return res.redirect(`${settings.webroot}/`);
   });
 
-  const rootserver = require('http').createServer(root);
-  rootserver.listen(PORT);
-  console.log(`SyncLounge WebApp successfully started on port ${PORT}`);
-  if (settings.webroot) {
-    console.log(`Running with base URL: ${settings.webroot}`);
-  }
+  app.listen(settings.webapp_port, () => {
+    console.log('SyncLounge WebApp successfully started on port ', settings.webapp_port);
+  });
+
+
+  console.log('Running with base URL: ', settings.webroot);
+
   if (settings.accessUrl) {
     console.log(`Access URL is ${settings.accessUrl}`);
     if (settings.webroot && !settings.accessUrl.includes(settings.webroot)) {
-      console.log(
-        `- WARNING: Your Access URL does not contain your webroot/WEBROOT setting: '${settings.webroot}'. Invite URLs may not work properly.`,
-      );
+      console.log('- WARNING: Your Access URL does not contain your webroot/WEBROOT setting: ',
+        settings.webroot, '. Invite URLs may not work properly.');
     }
   }
-  if (settings.authentication && settings.authentication.mechanism != 'none') {
+
+  if (settings.authentication && settings.authentication.mechanism !== 'none') {
     console.log('Authentication:', settings.authentication);
   }
+
   if (settings.servers) {
     console.log('Servers List:', settings.servers);
   } else if (settings.custom_server) {
@@ -163,7 +180,7 @@ const app = async (orm) => {
 
 bootstrap()
   .then((orm) => {
-    app(orm);
+    webApp(orm);
   })
   .catch((e) => {
     console.log('Error bootstrapping webapp:', e);
